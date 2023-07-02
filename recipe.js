6d1d32ea33c5b04ec963eaddc7c57c95b365e027
kimeunsoo0324 <nice_eunice@naver.com>
2023-07-03 04:58:09 +0900

    0703

---
+++ recipe.js
@@ -0,0 +1,9 @@
+//작성하기 버튼 누르면 박스 생성되고 글 작성하는 기능 구현해야함
+//찜(하트)버튼 누르면 하트가 테두리만 있도록 변해야 함, 다시 누르면 핑크 하트로 변해야 함
+//작성자 본인의 글에는 수정, 삭제 버튼 필요
+
+// const ggim=document.querySelector("#ggim i");
+
+// function ggim(){
+//     let menu=document.querySelector
+// }
