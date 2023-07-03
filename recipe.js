6a7cf3aedd51e4289675b40052934b69e5048f26
kimeunsoo0324 <nice_eunice@naver.com>
2023-07-04 00:26:58 +0900

    0704

--- recipe.js
+++ recipe.js
@@ -1,9 +1,17 @@
 //작성하기 버튼 누르면 박스 생성되고 글 작성하는 기능 구현해야함
-//찜(하트)버튼 누르면 하트가 테두리만 있도록 변해야 함, 다시 누르면 핑크 하트로 변해야 함
 //작성자 본인의 글에는 수정, 삭제 버튼 필요
 
-// const ggim=document.querySelector("#ggim i");
+window.onload = () => {
+    const likeButtons = document.querySelectorAll('.fa-regular.fa-heart');
 
-// function ggim(){
-//     let menu=document.querySelector
-// }
+    likeButtons.forEach((like) => {
+        const ggimButton = like.parentElement;
+
+        ggimButton.style.background = 'none';
+        like.classList.remove("fa-solid");
+
+        ggimButton.addEventListener("click", (e) => {
+            like.classList.toggle("fa-solid");
+        });
+    });
+}
