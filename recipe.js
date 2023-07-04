82ce642808be3f37ac49ec06133210e54bfbf89b
kimeunsoo0324 <nice_eunice@naver.com>
2023-07-05 02:04:20 +0900

    0705

--- recipe.js
+++ recipe.js
@@ -1,8 +1,21 @@
-// 새로 작성한 글에 css 적용해야 함, 하트 버튼도 적용해야 함(배경 색은 랜덤으로 하는지?)
-// 본인이 작성한 글에는 수정 버튼, 삭제 버튼 생성해야 함
+// window.onload = () => {
+//     const likeButtons = document.querySelectorAll('.fa-regular.fa-heart');
+
+//     likeButtons.forEach((like) => {
+//         const ggimButton = like.parentElement;
+
+//         ggimButton.style.background = 'none';
+//         like.classList.remove("fa-solid");
+
+//         ggimButton.addEventListener("click", (e) => {
+//             like.classList.toggle("fa-solid");
+//         });
+//     });
+// }
 
 window.onload = () => {
     const likeButtons = document.querySelectorAll('.fa-regular.fa-heart');
+    const menus = document.querySelectorAll('#menu1');
 
     likeButtons.forEach((like) => {
         const ggimButton = like.parentElement;
@@ -14,4 +27,15 @@ window.onload = () => {
             like.classList.toggle("fa-solid");
         });
     });
+
+    menus.forEach((menu) => {
+        const randomColor = getRandomColor();
+        menu.style.backgroundColor = randomColor;
+    });
+
+    function getRandomColor() {
+        const colors = ["#FFEDD2", "#E1D9FF", "#D9EDFF"];
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+    }
 }
