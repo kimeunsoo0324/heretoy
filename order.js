3431c8d8136843d0abd3e484248930488bd52ee5
kimeunsoo0324 <nice_eunice@naver.com>
2023-07-04 19:45:16 +0900

    0704

--- order.js
+++ order.js
@@ -8,33 +8,6 @@ var sum = function(){
     document.getElementById("total").innerText = totalprice + "원";
 }
 
-var count1=1;
-var count1A=function(){
-  count1 = count1 - 1;
-  if(count1<1){count1=1}
-  document.getElementById("result1").innerText=count1;
-  sum();
-}
-
-function count1B(){
-  count1 = count1 + 1;
-  document.getElementById("result1").innerText=count1;
-  sum();
-}
-
-var count2=1;
-var count2A=function(){
-  count2 = count2-1;
-  if(count2<1){count2=1}
-  document.getElementById("result2").innerText=count2;
-  sum();
-}
-
-var count2B=function(){
-  count2 = count2+1;
-  document.getElementById("result2").innerText=count2;
-  sum();
-}
 
 window.onload = function() {
     sum();
